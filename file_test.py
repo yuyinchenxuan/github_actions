58ac8307dfcb06becea2ad5655cac14a17187726
Ubuntu <ubuntu@ip-172-31-47-111.eu-west-1.compute.internal>
2025-09-15 01:02:51 +0000

    modified file_test.py

--- file_test.py
+++ file_test.py
@@ -3,7 +3,7 @@ import pytest
 def test_calc_addition():
   # Function test the output of 2+4
     output = 2+4
-    assert output == 6
+    assert output == 7
 
 def test_calc_substraction():
   # Function test the output of 2-4
